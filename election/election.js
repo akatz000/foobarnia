7bcea6678179bb3550a8630840aa68ef9995d484
Ariel Katz <captainkatz@gmail.com>
2015-12-09 23:50:05 -0500

    rough draft for election algorithm. needs refactoring!

--- election/election.js
+++ election/election.js
@@ -1,4 +1,67 @@
 exports.tally = function(votes) {
-  // TODO implement tally()
-  return []
+  var len = votes.length;
+  var majority = Math.floor( len / 2 ) + 1;
+  var votecount = {};
+  for (var i = 0;i < len;i++) {
+    var valueString = votes[i].toString();
+    if (votecount[valueString]) {
+      votecount[valueString] += 1;
+      if (votecount[valueString] >= majority) {
+        return [votes[i]];
+      }
+    }
+    else {
+      votecount[valueString] = 1;
+    }
+  }
+
+  var first = 0;
+  var second = 0;
+  var third = 0;
+
+  Object.keys(votecount).forEach(function(key) {
+    var value = votecount[key];
+    if (value > first) {
+      third = second;
+      second = first;
+      first = votecount[key];
+    }
+    else if (value == first) {}
+    else if (value > second) {
+      third = second;
+      second = value;
+    }
+    else if (value == second) {}
+    else if (value > third) {
+      third = value;
+    }
+  });
+
+  var results = []
+  Object.keys(votecount).forEach(function(key) {
+    if (votecount[key] == first) {
+      results.push(parseInt(key));
+    }
+  });
+  if (results.length >= 3) {
+    return results;
+  }
+
+  Object.keys(votecount).forEach(function(key) {
+    if (votecount[key] == second) {
+      results.push(parseInt(key));
+    }
+  });
+  if (results.length >= 3) {
+    return results;
+  }
+
+  Object.keys(votecount).forEach(function(key) {
+    if (votecount[key] == third) {
+      results.push(parseInt(key));
+    }
+  });
+  if (results.length >= 3) {
+    return results;
+  }
 }
--- election/election_test.js
+++ election/election_test.js
@@ -1,9 +1,20 @@
 var test = require('tape');
 var election = require('./election')
 
+// BROKEN: Candidate number five does NOT have majority of votes.
+//   Commented out and preserved for record.
+//
+// test('majority win', function (t) {
+//   t.plan(1);
+//   var votes = [2,2,5,1,5,7,5,5,1,5,2];
+//   var results = election.tally(votes);
+//   t.same(results, [5]);
+// });
+
+// Fixed majority test
 test('majority win', function (t) {
   t.plan(1);
-  var votes = [2,2,5,1,5,7,5,5,1,5,2];
+  var votes = [2,2,5,1,5,7,5,5,1,5,5];
   var results = election.tally(votes);
   t.same(results, [5]);
 });
@@ -11,6 +22,9 @@ test('majority win', function (t) {
 test('election runoff', function(t) {
   t.plan(1);
   var votes = [7,8,3,3,3,8,2,4,4,4,2,2,4,2,3,4,5,3,4,4];
+  // 2 >> 4
+  // 3 >> 5
+  // 4 >> 7
   var results = election.tally(votes);
   results.sort( function(a,b) { a - b } );
   t.same(results, [2,3,4]);
@@ -19,6 +33,10 @@ test('election runoff', function(t) {
 test('election runoff with tie', function(t) {
   t.plan(1);
   var votes = [8,9,1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,7,7,8];
+  // 1 >> 4
+  // 2, 3, 4, 5 >> 3
+  // 6, 7, 8 >> 2
+  // 9 >> 1
   var results = election.tally(votes)
   results.sort( function(a,b) { a - b } );
   t.same(results, [1,2,3,4,5]);
